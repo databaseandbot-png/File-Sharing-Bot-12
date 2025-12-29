abd6f44f2c38283dcb1edc44a72332d93d1a68e9
Davi Botz <databaseandbot@gmail.com>
2025-12-29 17:02:13 +0530

    Update CHANNEL_ID and FORCE_SUB_CHANNEL defaults

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ APP_ID = int(os.environ.get("APP_ID", "25237912"))
 API_HASH = os.environ.get("API_HASH", "e192dcc0926b4f447b8f28187f7e533b")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003439981785"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "5751033456"))
@@ -28,7 +28,7 @@ DB_URI = os.environ.get("DATABASE_URL", "")
 DB_NAME = os.environ.get("DATABASE_NAME", "Tom")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1003125658097"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", ""))
 JOIN_REQUEST_ENABLE = os.environ.get("JOIN_REQUEST_ENABLED", None)
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
