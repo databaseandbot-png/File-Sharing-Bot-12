41e0602fe0b69ed6cfde6838d925917d2b8ad980
Davi Botz <databaseandbot@gmail.com>
2025-12-29 20:51:25 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -8,7 +8,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/databaseandbot-png/File-Sharing-Bot-12'>Click here</a>\n○ Channel : @orewa_void\n○ Support Group : @Tom_Botz</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/databaseandbot-png/File-Sharing-Bot-12'>Click here</a>\n○ Channel : @orewa_void\n○ Support Group : @orewa_gaban</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
