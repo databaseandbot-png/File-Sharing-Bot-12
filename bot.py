b237a17a543a92cee179862a98cfa3fd8a741c03
Davi Botz <databaseandbot@gmail.com>
2025-12-29 21:32:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -59,7 +59,7 @@ class Bot(Client):
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/orewa_void for support")
+            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/orewa_franky for support")
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
